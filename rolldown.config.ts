aa066f6e7785830e883099715dda6cfc009c8b3e
qqfunc <qqfunc@gmail.com>
2025-08-15 14:05:50 +0900

    chore: set node platform

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -5,6 +5,7 @@ import del from 'rollup-plugin-delete'
 
 export default defineConfig({
   input: 'src/behavior_pack/scripts/index.ts',
+  platform: 'node',
   output: {
     dir: 'dist',
     entryFileNames: 'behavior_pack/scripts/[name].js',
